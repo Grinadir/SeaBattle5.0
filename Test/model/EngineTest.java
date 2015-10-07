9d650e901ddb48f2aa1ae1b4fe10895226c64911
Grinadir <zzzania683@gmail.com>
2015-10-07 22:59:05 +0300

    Code EngineTest.java. Find BUG

--- Test/model/EngineTest.java
+++ Test/model/EngineTest.java
@@ -188,6 +188,83 @@ public class EngineTest {
         assertTrue(map.getChoose().isFour());
     }
 
+    @Test
+    public void test(){
+        Engine engine = new Engine();
+        Map map = engine.getMap();
+
+        map.getChoose().chooseOne();
+        map.mainFunctionInMap(0,0);
+        map.mainFunctionInMap(2,0);
+        map.mainFunctionInMap(4,0);
+        map.mainFunctionInMap(6,0);
+
+        map.getChoose().chooseTwo();
+        map.mainFunctionInMap(0,2);
+        map.mainFunctionInMap(1,2);
+        map.mainFunctionInMap(3,2);
+        map.mainFunctionInMap(4,2);
+        map.mainFunctionInMap(6,2);
+        map.mainFunctionInMap(7,2);
+
+        map.getChoose().chooseThree();
+        map.mainFunctionInMap(0,4);
+        map.mainFunctionInMap(1,4);
+        map.mainFunctionInMap(2,4);
+        map.mainFunctionInMap(4,4);
+        map.mainFunctionInMap(5,4);
+        map.mainFunctionInMap(6,4);
+
+        map.getChoose().chooseFour();
+        map.mainFunctionInMap(1,7);
+        map.mainFunctionInMap(2,7);
+        map.mainFunctionInMap(3,7);
+        map.mainFunctionInMap(4,7);
+
+
+
+        assertEquals(
+                "S-S-S-S---\n" +
+                        "----------\n" +
+                        "DD-DD-DD--\n" +
+                        "----------\n" +
+                        "TTT-TTT---\n" +
+                        "----------\n" +
+                        "----------\n" +
+                        "-QQQQ-----\n" +
+                        "----------\n" +
+                        "----------\n", mapToString(map));
+
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                map.checkAndMarkMyField(x, y);
+
+                }
+            }
+
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                map.checkAndMarkMyField(x, y);
+
+            }
+        }
+//FIXME find BUG! Quadruple ship don,t marked when it attack
+        assertEquals(
+                "X0X0X0X000\n" +
+                        "0000000000\n" +
+                        "#X0#X0#X00\n" +
+                        "0000000000\n" +
+                        "##X0##X000\n" +
+                        "0000000000\n" +
+                        "0000000000\n" +
+                        "0###000000\n" +
+                        "0000000000\n" +
+                        "0000000000\n", mapToStringCheckFettle(map));
+
+
+
+    }
+
 
 
     private String mapToString(Map map) {
@@ -202,6 +279,33 @@ public class EngineTest {
                     buffer.append(type);
                 } else {
                     buffer.append('-');
+                }
+            }
+            buffer.append("\n");
+        }
+        return buffer.toString();
+    }
+
+    private String mapToStringCheckFettle(Map map) {
+        StringBuffer buffer = new StringBuffer();
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                Cell cell = map.getCellMY(x, y);
+                InterfaceShip ship = cell.getShip();
+                if (cell.getFettle().equals("ship")) {
+                    String shipType = ship.getClass().getSimpleName();
+                    char type = shipType.charAt(4);
+                    buffer.append(type);
+                }
+
+                else if (cell.getFettle().equals("dam")) {
+                    buffer.append('#');
+                }
+                else if (cell.getFettle().equals("kill")){
+                    buffer.append('X');
+                }
+                else {
+                    buffer.append('0');
                 }
             }
             buffer.append("\n");
